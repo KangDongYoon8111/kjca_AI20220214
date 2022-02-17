45c20987cfcba98433cd2abaaf15243d6d01f2fe
kmy7102 <99640720+kmy7102@users.noreply.github.com>
2022-02-17 16:37:13 +0900

    Add files via upload

---
+++ Chapter4/KimYeongMin_Chapter4.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KimYeongMin_Chapter4 : MonoBehaviour
+{
+    //1 i++와 ++i의 차이점은 무엇인가요?
+    //i++는 출력 후 1을 더하고 ++i는 더한 값이 출력된다
+
+    //2. 각 출력문의 값을 적으세요
+    //int i = 1;
+    //Debug.Log(i = i + 1); 답: 2
+    //Debug.Log(i++); 답:2
+    //Debug.Log(++i); 답:4
+    //Debug.Log(i += 1); 답:5
+
+    //3 다음 코드에서 b는 어떤 값을 가질까요?
+    //int a = 10;
+    //string b = a == 0 ? "가나다" : "ABC";
+    // ABC
+}
