b77c668f9f6fc9c9d31c3ebe4749936ed98d6749
dprnd <99640002+dprnd@users.noreply.github.com>
2022-02-17 16:30:28 +0900

    Add files via upload

---
+++ Chapter4/HongJaRang_Chapter4.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HongJaRang_Chapter4 : MonoBehaviour
+{
+    // i++와 ++i의 차이점은 무엇인가요?
+    // i++은 i값이 먼저 나오고 계산이 이루어지고 ++i는 계산이 이루어지고 값이 표현됨
+
+    // int i = 1;
+    //Debug.Log(i = i + 1); 답 : 2
+    //Debug.Log(i++); 답 : 2
+    //Debug.Log(++i); 답 : 4
+    //Debug.Log(i += 1); 답 : 5
+
+    // int a = 10;
+    // string b = a == 0 ? "가나다" : "ABC";
+    // ABC
+    
+}
