2db667c29f766827cb2fe196691f6b210513e3ef
choiyounghwa0614 <99639377+choiyounghwa0614@users.noreply.github.com>
2022-02-17 16:32:41 +0900

    Add files via upload

---
+++ Chapter4/choiyhounhwa_Chapter4.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //  int i =1;       답:1
+        // Debug.Log(i+i+1); 답:3
+
+       //Debug.Log(i++); 답:4
+        // Debug.Log(++i); 답:6
+        // Debug.Log(i+=1); 답:1
+
+        //int a =10;
+        //string b =a== 0 ? "가나다" : "ABC"; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
