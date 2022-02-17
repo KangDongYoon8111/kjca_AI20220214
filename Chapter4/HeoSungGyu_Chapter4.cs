f1a69ecce255e8873db809836fca61dc13cdfe88
sunggyu74 <99639216+sunggyu74@users.noreply.github.com>
2022-02-17 16:35:01 +0900

    Add files via upload

---
+++ Chapter4/HeoSungGyu_Chapter4.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeoSungGyu_Chapter4 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //1. i++와 ++i의 차이점은 무엇인가요? 
+        //   i++ : i값 변수지정 후에 더하기 ++i : i값 변수에 먼저 더하고 메모리에 저장
+        //2. 각 출력문의 값을 적으세요.
+        //  int i = 1; 
+        //  Debug.Log(i = i + 1);       // 2
+        //  Debug.Log(i++);             // 2
+        //  Debug.Log(++i);             // 4
+        //  Debug.Log(i += 1);          // 5
+        //3. 다음 코드에서 b는 어떤 값을 가질까요?
+        //  int a = 10;
+        //  string b = a == 0 ? "가나다" : "ABC";  //Debug.Log(b); //"ABC"
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
