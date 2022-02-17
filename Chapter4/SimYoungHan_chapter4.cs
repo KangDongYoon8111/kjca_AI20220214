8ab29d1bf02cd704d74590e4cc7a0a8b1f8c8ab3
tladud123 <99639268+tladud123@users.noreply.github.com>
2022-02-17 16:41:39 +0900

    Add files via upload

---
+++ Chapter4/SimYoungHan_chapter4.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimYoungHan_chapter4 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+        // i++와 ++i 차이점은 무엇인가요?
+        // 답: i++은 출력후 실행 ++i는 실행후 출력
+        // int i = 1;
+        // debug.log(i = i +1): 답: 2
+        // debug.log(i++);      답: 2
+        // debug.log(++i):      답: 4
+        // debug.log(i +=1);    답: 5
+        
+        // int a = 10;
+        //string b = a == 0 ? "가나다 :ABC";
+        // 답: ABC
+
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
