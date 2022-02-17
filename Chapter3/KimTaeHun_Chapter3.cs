42a3a7e28335b2f66978ade01e69eaf3f21d0f35
Moonkal <99639280+Moonkal@users.noreply.github.com>
2022-02-17 14:10:42 +0900

    Add files via upload

---
+++ Chapter3/KimTaeHun_Chapter3.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KimTaeHun_Chapter3 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string a = "30";
+        string b = "40";
+        int i = int.Parse(a);
+        int j = int.Parse(b);
+        Debug.Log(i*j);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
