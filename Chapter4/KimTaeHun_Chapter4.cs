d887c01cd09c11869741577618ef237c38068310
Moonkal <99639280+Moonkal@users.noreply.github.com>
2022-02-17 16:30:15 +0900

    Add files via upload

---
+++ Chapter4/KimTaeHun_Chapter4.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KimTaeHun_Chapter4 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*
+         1.i++은 후위 증감연산자이고 ++i는 전위 증감연산자입니다.
+
+         */
+        /*
+         2.답:2;
+            답:2
+        답:4
+        답:5
+         */
+        /*
+         답:ABC
+         */
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
