c02949180971994e2667d3df7abfc3a1a36f2053
choi5907 <99639267+choi5907@users.noreply.github.com>
2022-02-17 16:31:01 +0900

    Add files via upload

---
+++ Chapter4/Choisuil_Chapter4.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Choisuil_Chapter4 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 1. i++ 출력 후 덧셈, ++i 출력 전 덧셈
+
+        // 2. 각 출력문의 값을 적으세요.
+        //int i = 1;
+        //Debug.Log(i = i + 1);   답: 2
+        //Debug.Log(i++);          답: 2
+        //Debug.Log(++i);          답: 4
+        //Debug.Log(i += 1);      답: 5
+
+        // 3. 다음 코드에서 b는 어떤 값을 가질까요?
+        //int a = 10;
+        //string b = a == 0 ? "가나다" : "ABC";
+        // 답: ABC
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
