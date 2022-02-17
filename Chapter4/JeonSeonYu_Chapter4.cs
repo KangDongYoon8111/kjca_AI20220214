c33561070d392aa7d5b44e2c2a6f01d95200e817
NOESYU <86655940+NOESYU@users.noreply.github.com>
2022-02-17 16:29:59 +0900

    Add files via upload

---
+++ Chapter4/JeonSeonYu_Chapter4.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JeonSeonYu_Chapter4 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //1. i++와 i++의 차이점은 무엇인가요?
+        //후위연산는 기존 변수값이 먼저 출력한 뒤 변수 값이 증가 되고, 전위연산자는 먼저 증가 계산을 한뒤 변수를 출력한다.
+
+        //2. 각 출력문의 값을 적으세요.
+        //int i = 1;
+        //Debug.Log(i = i + 1); 답: 2
+        //Debug.Log(i++); 답: 2
+        //Debug.Log(++i); 답: 4
+        //Debug.Log(i += 1); 답: 5
+
+        //3. 다음 코드에서 b는 어떤 값을 가질까요?
+        //int a = 10;
+        //string b = a == 0 ? "가나다" : "ABC";
+        //답: ABC
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
