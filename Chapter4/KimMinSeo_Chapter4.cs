a04a0160449622c3e5f0aad9ffefac439d0b4330
MinSeo <86696817+gyullo18@users.noreply.github.com>
2022-02-17 16:31:21 +0900

    Add files via upload

---
+++ Chapter4/KimMinSeo_Chapter4.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KimMinSeo_Chapter4 : MonoBehaviour
+{
+    //1. i++와 ++i의 차이점은 무엇인가요?
+    //   i++ : i를 표시하고 연산
+    //   ++i : 연산 후 i를 표시
+
+    //2. 각 출력문의 값을 적으세요.
+    //    int i = 1;
+    //    Debug.Log(i = i + 1); 답 : 2
+    //    Debug.Log(i++); 답 : 2
+    //    Debug.Log(++i); 답 : 4
+    //    Debug.Log(i += 1); 답 : 5
+
+    //3. 다음 코드에서 b는 어떤 값을 가질까요? 
+    //    int a = 10;
+    //    string b = a == 0 ? "가나다" : "ABC";
+    //    답 : ABC
+
+}
