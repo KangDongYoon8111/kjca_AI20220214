27d36af1da42085c6f9bf44b70d9bd5e46475859
tladud123 <99639268+tladud123@users.noreply.github.com>
2022-02-17 14:30:11 +0900

    Add files via upload

---
+++ Chapter3/SimYoungHan_chapter3.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimYoungHan_chapter3 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string a = "30";
+        string b = "40";
+
+        Debug.Log($"사각형의 너비는{a}");
+        Debug.Log($"사각형의 높이는{b}");
+        Debug.Log($"사각형의 넓이는 : {int.Parse(a) * int.Parse(b)} ");
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
