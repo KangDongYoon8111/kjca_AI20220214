ae1a78ea41bb2b632fe079b01716026af709f606
seoido <99639245+seoido@users.noreply.github.com>
2022-02-17 16:32:40 +0900

    Add files via upload

---
+++ Chapter4/youngjunChapter4.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class youngjunChapter4 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 1
+        // i++ 는 먼저 출력후 덧셈,++i 는i 출력전 덧셈
+
+        // 2
+        // int i = 1;
+        // Debug.Log(i = i + 1); 답 :2
+        // Debug.Log(i++); 답:2
+        // Debug.Log(++i); 답:4
+        // Debug.Log(i += 1);5
+
+        // 3
+        // int a = 10;
+        // string b = a == 0 ? "가나다" : "ABC";
+        // 답 : ABC 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
