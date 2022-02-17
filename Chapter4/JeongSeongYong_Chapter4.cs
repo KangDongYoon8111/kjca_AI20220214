cecabbebd2a2ad3d6c54915ae9a9f6244f6b8146
JeongSeongYong <99639179+JeongSeongYong@users.noreply.github.com>
2022-02-17 16:34:53 +0900

    Add files via upload

---
+++ Chapter4/JeongSeongYong_Chapter4.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JeongSeongYong_Chapter4 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    //1. i++와 ++i의 차이점은 무엇인가요?
+    // Debug.Log(); 를 하게 되면 i++ 는 i를 출력 후 1을 더하기 때문에 콘솔에는 i값만 나오게 된다.
+    // ++i 는 1을 더한 값이 콘솔에 출력된다.
+    //2. 각 출력문의 값을 적으세요
+    //   int i = 1;
+    //Debug.Log(i=i+1);  답:2
+    //Debug.Log(i++);    답:2
+    //Debug.Log(++i);    답:4
+    //Debug.Log(i+=1);   답:5
+
+}
+}
+
+
+
+    //3. 다음 코드에서 b는 어떤 값을 가질까요?
+    //int a= 10;
+    //string b=a==0? "가나다" : "ABC";
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
