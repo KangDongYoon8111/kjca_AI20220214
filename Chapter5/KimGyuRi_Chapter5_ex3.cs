57830f5a95e5fec695a678b1b9b8970e68bd0570
BBIBBI1i <99639273+BBIBBI1i@users.noreply.github.com>
2022-02-18 14:45:23 +0900

    Add files via upload

---
+++ Chapter5/KimGyuRi_Chapter5_ex3.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KimGyuRi_Chapter5_ex3 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string userInput = "16";
+        int userScore = int.Parse(userInput);
+
+        if (userScore % 3 == 0)
+        {
+            Debug.Log("3의 배수 입니다");
+        }
+        else
+        {
+            Debug.Log("3의 배수가 아닙니다");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
