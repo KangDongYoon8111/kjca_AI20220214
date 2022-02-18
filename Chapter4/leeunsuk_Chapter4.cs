2a3dc30d73bd7e9dc8be4efce71431c060b84426
ceekcole1 <99639229+ceekcole1@users.noreply.github.com>
2022-02-18 14:56:28 +0900

    Update leeunsuk_Chapter4.cs

--- Chapter4/leeunsuk_Chapter4.cs
+++ Chapter4/leeunsuk_Chapter4.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 
 public class leejunsuk_Chapter4 : MonoBehaviour
 {
-    //1. i++와 i++의 차이점은 무엇인가요?
-    //후위연산는 기존 변수값이 먼저 출력한 뒤 변수 값이 증가 되고, 전위연산자는 먼저 증가 계산을 한뒤 변수를 출력한다.
+    // Start is called before the first frame update
+    void Start()
+    {
+        //1. i++와 ++i의 차이점은 무엇인가요? 
+        //   i++ : i값 변수지정 후에 더하기 ++i : i값 변수에 먼저 더하고 메모리에 저장
+        //2. 각 출력문의 값을 적으세요.
+        //  int i = 1; 
+        //  Debug.Log(i = i + 1);       // 2
+        //  Debug.Log(i++);             // 2
+        //  Debug.Log(++i);             // 4
+        //  Debug.Log(i += 1);          // 5
+        //3. 다음 코드에서 b는 어떤 값을 가질까요?
+        //  int a = 10;
+        //  string b = a == 0 ? "가나다" : "ABC";  //Debug.Log(b); //"ABC"
+    }
 
-    //2. 각 출력문의 값을 적으세요.
-    //int i = 1;
-    //Debug.Log(i = i + 1); 답: 2
-    //Debug.Log(i++); 답: 2
-    //Debug.Log(++i); 답: 4
-    //Debug.Log(i += 1); 답: 5
+    // Update is called once per frame
+    void Update()
+    {
 
-    //3. 다음 코드에서 b는 어떤 값을 가질까요?
-    //int a = 10;
-    //string b = a == 0 ? "가나다" : "ABC";
-    //답: ABC
+    }
 }
+
