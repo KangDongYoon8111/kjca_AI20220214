bd4147fcfe6e54e167dd421fbb8d2ca6c56f11a0
kacard123 <99639271+kacard123@users.noreply.github.com>
2022-02-18 16:44:56 +0900

    Add files via upload

---
+++ Chapter5/LimJuyeong_Chapter6_ex6.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LimJuyeong_Chapter6_ex6 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string userInput1 = "10";
+        string userInput2 = "2";
+        string userInput3 = "/";
+
+        int input1 = int.Parse(userInput1);
+        int input2 = int.Parse(userInput2);
+        int value = 0;
+
+        switch(userInput3)
+        {
+            case "+":
+                value = input1 + input2;
+                break;
+            case "-":
+                value = input1 - input2;
+                break;
+            case "*":
+                value = input1 * input2;
+                break;
+            case "/":
+                value = input1 / input2;
+                break;
+            case "%":
+                value = input1 % input2;
+                break;
+        }
+        Debug.Log($"입력하신 {input1}{userInput3}{input2}의 값은 {value}입니다.");
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
