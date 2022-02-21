f45daeef12dc2b92d279af2dbb41d66cdca307b6
ceekcole1 <99639229+ceekcole1@users.noreply.github.com>
2022-02-21 17:16:06 +0900

    Add files via upload

---
+++ Chapter5/leejunsuk_Chaper5_ex9.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class leejunsuk_Chaper5_ex9 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int sum = 0;
+
+        for (int i = 1; i < 11; i++)
+        {
+            if (i % 3 == 0)
+            {
+                continue;
+            }
+            Debug.Log(i);
+            sum += i;
+            }
+        Debug.Log($"1~10중 3으로 나누어 떨어지지 않는 수의 합 : {sum}");
+        }
+            
+
+
+
+        
+       
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
